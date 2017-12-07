2a1f2b0efc9c5db501fd63ade7e99975d7395b09
camneu37 <neuner.camille@gmail.com>
2017-12-06 22:59:46 -0800

    def scraper method to get list of all series

--- lib/what_to_watch/scraper.rb
+++ lib/what_to_watch/scraper.rb
@@ -1,7 +1,13 @@
 class WhatToWatch::Scraper
 
-  def scrape_series_showtime
+  def scrape_series_list
     doc = Nokogiri::HTML(open("http://www.sho.com/series"))
+    shows = doc.css("li.promo")
+    all_shows = []
+    shows.each do |show|
+      all_shows << show.css("a.promo__link").text
+    end
+    all_shows
   end
 
 end
