9e452bc908b02b75222d8dcbc7e7b4d9bff1d213
camneu37 <neuner.camille@gmail.com>
2017-12-10 20:49:37 -0800

    add method for adding show attributes to show instances

--- lib/what_to_watch/shows.rb
+++ lib/what_to_watch/shows.rb
@@ -1,7 +1,27 @@
 class WhatToWatch::Shows
   attr_accessor :name, :about, :airs, :episodes
+  @@all = []
 
-  
+  def initialize(name)
+    @name = name
+    @@all << self
+    add_show_attributes
+  end
+
+  def add_show_attributes
+    show_link = "http://www.sho.com/#{self.name.downcase.gsub(" ", "-")}"
+    show = WhatToWatch::Scraper.new.scrape_show_details(show_link)
+    @about = show[:about]
+    @airs = show[:airs]
+    @episodes = show[:episodes]
+  end
+
+  def self.create_from_list
+    series_list.each do |show|
+      WhatToWatch::Shows.new(show)
+    end
+    binding.pry
+  end
 
   def self.list_all
     series_list.uniq.sort.each.with_index(1) do |show, i|
@@ -13,6 +33,8 @@ class WhatToWatch::Shows
     WhatToWatch::Scraper.new.scrape_series_list
   end
 
-
+  def self.all
+    @@all
+  end
 
 end
