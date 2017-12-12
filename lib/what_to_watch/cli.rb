ced474520dabbb851c44533d99728f5f4e930cb9
camneu37 <neuner.camille@gmail.com>
2017-12-11 19:32:59 -0800

    start defining cli start method - add welcome message and show list of series

--- lib/what_to_watch/cli.rb
+++ lib/what_to_watch/cli.rb
@@ -6,5 +6,14 @@ class WhatToWatch::CLI
     end
   end
 
+  def self.start
+    WhatToWatch::Shows.create_from_list
+    puts "Welcome to What to Watch!"
+    puts "Below you'll see a list of all available series from Showtime."
+    puts "Please enter the number of the show for which you'd like more information."
+    list_all_shows
+    binding.pry
+    input = gets.strip
+  end
 
 end
