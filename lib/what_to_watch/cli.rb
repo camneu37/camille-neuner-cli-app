00fbadffaa70e61977cadce3dabc545fee5e8c44
camneu37 <neuner.camille@gmail.com>
2017-12-11 19:50:33 -0800

    add code to show series details upon user input

--- lib/what_to_watch/cli.rb
+++ lib/what_to_watch/cli.rb
@@ -10,10 +10,26 @@ class WhatToWatch::CLI
     WhatToWatch::Shows.create_from_list
     puts "Welcome to What to Watch!"
     puts "Below you'll see a list of all available series from Showtime."
-    puts "Please enter the number of the show for which you'd like more information."
     list_all_shows
-    binding.pry
-    input = gets.strip
+    puts "Please enter the number of the show for which you'd like more information."
+    input = gets.strip.to_i
+    WhatToWatch::Shows.all.sort_by{|s| s.name}.each.with_index(1) do |show, i|
+      if input != i
+      elsif input == i
+        puts " "
+        puts "#{show.name}"
+        puts " "
+        puts "About the show: "
+        puts " "
+        puts "#{show.about}"
+        puts " "
+        puts "There are #{show.episodes} available to stream on Showtime Anytime"
+        puts " "
+        if show.airs != "No airings currently scheduled"
+          puts "This show will air on Showtime's channel #{show.airs}"
+        end
+      end
+    end
   end
 
 end
