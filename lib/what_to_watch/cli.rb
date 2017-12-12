44ca26f2952cdc19faf95c04d8151418caa09a57
camneu37 <neuner.camille@gmail.com>
2017-12-11 20:04:19 -0800

    separate code for printing a show's details into its own method

--- lib/what_to_watch/cli.rb
+++ lib/what_to_watch/cli.rb
@@ -6,6 +6,21 @@ class WhatToWatch::CLI
     end
   end
 
+  def self.show_series_details(show)
+    puts " "
+    puts "#{show.name}"
+    puts " "
+    puts "About the show: "
+    puts " "
+    puts "#{show.about}"
+    puts " "
+    puts "There are #{show.episodes} available to stream on Showtime Anytime"
+    puts " "
+    if show.airs != "No airings currently scheduled"
+      puts "This show will air on Showtime's channel #{show.airs}"
+    end
+  end
+
   def self.start
     WhatToWatch::Shows.create_from_list
     puts "Welcome to What to Watch!"
@@ -13,22 +28,6 @@ class WhatToWatch::CLI
     list_all_shows
     puts "Please enter the number of the show for which you'd like more information."
     input = gets.strip.to_i
-    WhatToWatch::Shows.all.sort_by{|s| s.name}.each.with_index(1) do |show, i|
-      if input != i
-      elsif input == i
-        puts " "
-        puts "#{show.name}"
-        puts " "
-        puts "About the show: "
-        puts " "
-        puts "#{show.about}"
-        puts " "
-        puts "There are #{show.episodes} available to stream on Showtime Anytime"
-        puts " "
-        if show.airs != "No airings currently scheduled"
-          puts "This show will air on Showtime's channel #{show.airs}"
-        end
-      end
     end
   end
 
