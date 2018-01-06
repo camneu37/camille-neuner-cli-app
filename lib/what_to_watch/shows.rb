a13d5f814e36f0b7331fb70a1a460bce23ab231c
camneu37 <neuner.camille@gmail.com>
2018-01-06 15:54:12 -0800

    edit list methods

--- lib/what_to_watch/shows.rb
+++ lib/what_to_watch/shows.rb
@@ -42,14 +42,14 @@ class WhatToWatch::Shows
 
   def self.list_all
     sorted_shows.each.with_index(1) do |show, i|
-      puts "#{i}. #{show}"
+      puts "#{i}. #{show.name}"
     end
   end
 
   def self.list_num
     sorted_shows.each.with_index(1) do |show, i|
       if show.name.start_with?("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
-        puts "#{i}. #{show}"
+        puts "#{i}. #{show.name}"
       end
     end
   end
@@ -57,7 +57,7 @@ class WhatToWatch::Shows
   def self.list_a_to_f
     sorted_shows.each.with_index(1) do |show, i|
       if show.name.start_with?("A", "B", "C", "D", "E", "F")
-        puts "#{i}. #{show}"
+        puts "#{i}. #{show.name}"
       end
     end
   end
